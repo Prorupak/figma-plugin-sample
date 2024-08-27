58c1821e0ea775fabda46f56e6e3af075d9606a3
Prorupak <rupaksapkota2000@gmail.com>
2024-08-27 19:34:58 +0545

    feat: Set up basic plugin structure

---
+++ plugin/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+
+const path = require("path");
+
+module.exports = {
+  // mode: "development",
+  entry: {
+    ui: "./src/ui/App.tsx",
+    code: "./src/code.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", "json"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/ui/index.html",
+      filename: "ui.html",
+      inlineSource: ".(js|css)$",
+      chunks: ["ui"],
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+  ],
+};
