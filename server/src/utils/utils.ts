55f1810caa0f52d229ae0e99f682fee3b23fe49b
Prorupak <rupaksapkota2000@gmail.com>
2024-08-27 22:32:47 +0545

    Fix: Load environment variables

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,5 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 export const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
 export const FIGMA_CLIENT_ID = process.env.FIGMA_CLIENT_ID;
