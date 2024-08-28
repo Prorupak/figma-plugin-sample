4b171e7e8dd7e0a71b530da215ecddb56d6cb6cb
Prorupak <rupaksapkota2000@gmail.com>
2024-08-28 11:25:22 +0545

    feat: add useDebounceCallback hook

---
+++ plugin/src/hooks/useDebounce.ts
@@ -0,0 +1,40 @@
+import { useCallback, useRef } from "react";
+
+/**
+ * useDebounceCallback hook
+ *
+ * Returns a debounced version of a function that delays invoking it until after a specified delay.
+ *
+ * @param callback - The function to debounce.
+ * @param delay - The delay in milliseconds after which the function should be invoked.
+ * @returns A debounced function.
+ *
+ * @example
+ * const debounce = useDebounceCallback((newQuery: string) => {
+ *   setQuery(newQuery);
+ *   setResults([]);
+ *   setCurrent(undefined);
+ *   setSearching(newQuery !== '');
+ *   parent.postMessage({ pluginMessage: { query: newQuery } }, '*');
+ * }, 300);
+ */
+export function useDebounceCallback<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number
+) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedCallback = useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      timeoutRef.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
+
+  return debouncedCallback;
+}
